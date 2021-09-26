66fc7b2b866ad1d5318c2dd07d899474503684b5
JOY_SARKAR <joysarkarbd407@gmail.com>
2021-09-26 22:09:51 +0600

    Final modification

--- src/components/Invitation/Invitation.js
+++ src/components/Invitation/Invitation.js
@@ -9,17 +9,28 @@ const Invitation = (props) => {
     const total = invited.reduce(totalFees, 0)
 
     return (
-        <div className="card bg-light mb-3 text-left">
-            <div className="card-header text-secondary"><h3>Invitation list</h3></div>
-            <div className="card-body text-secondary">
-                <h4 className="card-title">Invited: {invited.length}</h4>
-                <h5>Total fees:<span className=" text-primary"> ${total}</span></h5>
-                <h6>Invited Writers:</h6>
-                <ul>{
-                    invited.map(writerName => <li>{writerName.name}</li>)
-                }
-                </ul>
+        <div className="sticky-top">
+            <div className="card bg-light mb-3 text-left">
+                <div className="card-header text-secondary"><h3>Invitation list</h3></div>
+                <div className="card-body text-secondary">
+                    <h4 className="card-title">Invited: {invited.length}</h4>
+                    <h5>Total fees:<span className=" text-primary"> ${total}</span></h5>
+                </div>
             </div>
+            {
+                invited.map(writerName => <div className="card mb-3" >
+                    <div className="row no-gutters">
+                        <div className="col-md-4">
+                            <img src={writerName.img} className="card-img" alt={writerName.img} />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">{writerName.name}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
